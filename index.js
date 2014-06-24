6129e7f99d73d238b0b81c9072be944dc920fff0
Constantin Titarenko <constantin.titarenko@gmail.com>
2014-06-24 23:29:35 +0300

    Simplified API

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 var pg = require('pg');
 var Q = require('q');
-var _ = require('lodash');
-
-var sprintf = require('util').format;
-var uri = require('../config').db.uri;
 
 function pluckId (row) {
 	return +row.id;
@@ -16,114 +12,49 @@ function SqlError (args, error) {
 	this.message = 'Can\'t complete SQL query due to error.';
 }
 
-function query () {
-	var deferred = Q.defer();
-	var args = Array.prototype.slice.call(arguments);
-	
-	pg.connect(methods.uri, function (error, client, done) {
-		if (error) {
-			return deferred.reject(new SqlError(args, error));
+function mysqlToPg (sql) {
+	var index = 1;
+	return sql.replace(/\?/g, function () {
+		return '$' + index++;
+	});
+}
+
+function ctor (connectionParameters) {
+	return function query () {
+		var deferred = Q.defer();
+		var args = Array.prototype.slice.call(arguments);
+		
+		var isInsert = args[0].indexOf('insert into') == 0;
+		if (isInsert) {
+			args[0] += ' returning id';
 		}
 
-		args.push(function (error, result) {
-			done();
+		args[0] = mysqlToPg(args[0]);
+		
+		pg.connect(connectionParameters, function (error, client, done) {
 			if (error) {
-				deferred.reject(new SqlError(args, error));
-			} else {
-				deferred.resolve(result.rows);
+				return deferred.reject(new SqlError(args, error));
 			}
+
+			args.push(function (error, result) {
+				done();
+				if (error) {
+					deferred.reject(new SqlError(args, error));
+				} else {
+					deferred.resolve(result.rows);
+				}
+			});
+
+			client.query.apply(client, args);
 		});
 
-		client.query.apply(client, args);
-	});
+		var result = deferred.promise;
+		if (isInsert) {
+			result = result.then(pluckId);
+		}
 
-	return deferred.promise;
+		return result;
+	};
 }
 
-function querySingle () {
-	return query.apply(this, arguments).then(function (rows) {
-		return rows && rows.length && rows[0];
-	});
-}
-
-function insert (table, row) {
-	_.extend(row, {
-		created_at: new Date(),
-		creator_id: this.user.id
-	});
-
-	var names = _.keys(row).join(', ');
-	var values = _.values(row);
-	var placeholders = values.map(function (item, index) {
-		return '$' + (index + 1);
-	}).join(', ');
-
-	var sql = sprintf(
-		'insert into %s (%s) values (%s) returning id',
-		table, names, placeholders);
-
-	return querySingle(sql, values).then(pluckId);
-}
-
-function updateInternal (table, row) {
-	if (!row.id) {
-		throw new Error('Can\'t update row without id.');
-	}
-
-	var params = [row.id];
-	delete row.id;
-	
-	var names = _.keys(row).filter(function (key) {
-		return key !== 'id';
-	}).map(function (key, index) {
-		params.push(row[key]);
-		return key + ' = $' + (index + 2);
-	}).join(', ');
-
-	var sql = sprintf('update %s set %s where id = $1 returning id', table, names);
-	return querySingle(sql, params).then(pluckId);
-}
-
-function update (table, row) {
-	return updateInternal.call(this, table, _.extend(row, {
-		updated_at: new Date(),
-		updater_id: this.user.id
-	}));
-}
-
-function remove (table, id) {
-	return updateInternal.call(this, table, {
-		id: id,
-		remover_id: this.user.id,
-		removed_at: new Date()
-	}).then(function () {
-		return querySingle('delete from ' + table + ' where id = $1', [id]);
-	});
-}
-
-function find (table, id) {
-	return querySingle('select * from ' + table + ' where id = $1', [id]);
-}
-
-function end () {
-	pg.end();
-}
-
-var methods = {
-	query: query,
-	querySingle: querySingle,
-	insert: insert,
-	update: update,
-	remove: remove,
-	find: find,
-	end: end
-};
-
-try {
-	require.resolve('./config');
-	methods.uri = require('./config').db.uri;
-} catch (e) {
-	methods.uri = process.env.PG_URI || '';
-}
-
-module.exports = methods;
+module.exports = ctor;
